748ae88c03733a06f9cc61470f850fabdff6360b
Filip-Rak <73279456+Filip-Rak@users.noreply.github.com>
2024-05-20 12:36:28 +0200

    delete useless import

--- src/main/java/kosmo/pathfinding/MainApplication.java
+++ src/main/java/kosmo/pathfinding/MainApplication.java
@@ -3,7 +3,6 @@ package kosmo.pathfinding;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
