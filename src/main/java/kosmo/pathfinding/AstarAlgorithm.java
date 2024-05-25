9adb1a15da3d48c82d789a93cd67a5b3b5bd37f9
arek <areksa123@o2.pl>
2024-05-25 19:58:05 +0200

    finally working variant of A*, no strings attached

--- src/main/java/kosmo/pathfinding/AstarAlgorithm.java
+++ src/main/java/kosmo/pathfinding/AstarAlgorithm.java
@@ -1,8 +1,6 @@
 package kosmo.pathfinding;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 import static kosmo.pathfinding.State.*;
 
@@ -19,7 +17,9 @@ public class AstarAlgorithm implements Runnable {
     private static class Node implements Comparable<Node>
     {
         int x, y;
-        double cost = 0;
+        double q = 0;
+        double h = 0;
+        double f = 0;
         Node prev ;
         Node(int x, int y, Node prev)
         {
@@ -30,39 +30,12 @@ public class AstarAlgorithm implements Runnable {
 
         @Override
         public String toString() {
-            return "( x: "+x+", y: "+y+" )";
-        }
-
-        public void setPrev(Node prev) {
-            this.prev = prev;
-        }
-
-        public void setCost(Node start, Node finish) {
-//            double px = Math.abs(start.x - finish.x);
-//            double py = Math.abs(start.y - finish.y);
-//            if(px != 0 && py != 0)
-//                this.cost = Math.pow(this.x - finish.x, 2)/px + Math.pow(this.y - finish.y, 2)/py;
-//            else
-            this.cost= Math.min(Math.abs(this.x-start.x),Math.abs(this.y-start.y));
-            this.cost+= Math.sqrt(Math.pow(this.x-finish.x,2)+Math.pow(this.y-finish.y,2));
-
-
-        }
-//        public void setCost( Node startNode, Node finishNode, double h_val) {
-//
-//            this.cost = (double) Math.abs(startNode.x - this.x)/  + (double) Math.abs(startNode.y - this.y)  +h_val;
-//        }
-        public double getCost() {
-            return cost;
+            return "( x: " +x+ "; y: "+ y+ "; f: "+f+" )";
         }
 
         @Override
         public int compareTo(Node o) {
-            if(x == o.x && y == o.y)
-                return 0;
-            if(x==o.x)
-                return -1;
-            return 1;
+            return (int)(this.f - o.f);
         }
     }
     @Override
@@ -71,23 +44,23 @@ public class AstarAlgorithm implements Runnable {
         Node lastNode = null;
         Node startNode = null;
         Execution.get().startPoint();
-        int[][] tab_pos = new int[rows][cols];
+        int[][] types = new int[rows][cols];
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < cols; j++) {
                 if (gridSquares[i][j].getState() == ORIGIN) {
-                    tab_pos[i][j] = 2;
+                    types[i][j]=0;
                     startNode = new Node(j, i, null);
                 }
                 else if( gridSquares[i][j].getState() == DESTINATION){
-                    tab_pos[i][j] = 10;
+                    types[i][j]=0;
                     lastNode = new Node(j, i, null);
                 }
                 else if (gridSquares[i][j].getState() == OBSTACLE){
-                    tab_pos[i][j] = 2;
+                    types[i][j]=5;
                 }
                 else {
-                    tab_pos[i][j] = 0;
+                    types[i][j]=0;
                 }
             }
         }
@@ -97,137 +70,94 @@ public class AstarAlgorithm implements Runnable {
             Execution.get().stopPoint();
             return;
         }
-        ArrayList<Node> tochk = new ArrayList<>(List.of(new Node[]{
-                new Node(startNode.x - 1, startNode.y, startNode),
-                new Node(startNode.x + 1, startNode.y,startNode),
-                new Node(startNode.x, startNode.y - 1,startNode),
-                new Node(startNode.x, startNode.y + 1,startNode)
-        }));
+
+        PriorityQueue<Node> open = new PriorityQueue<>(Comparator.comparingDouble(node -> node.f));
+        open.add(startNode);
+
         boolean foundPath = false;
-        gridSquares[startNode.y][startNode.x].setState(VISITED);
-        Execution.get().Wait();
-        for (int i =0; i < tochk.size(); i++) {
-            if(tochk.get(i).x > -1 && tochk.get(i).y > -1 && tochk.get(i).x < cols && tochk.get(i).y < rows ){
-                if(tab_pos[tochk.get(i).y][tochk.get(i).x] == 10)
-                {
-                    lastNode.prev = startNode;
-                    foundPath = true;
-                }
-                else if(tab_pos[tochk.get(i).y][tochk.get(i).x] == 0){
-                    gridSquares[tochk.get(i).y][tochk.get(i).x].setState(FRONTIER);
-                    tab_pos[tochk.get(i).y][tochk.get(i).x] = 1;
-                    Execution.get().Wait();
-                    tochk.get(i).setCost(startNode,lastNode);
-                }
-                else {
-                    tochk.remove(i);
-                    i--;
-                }
-
-            }
-            else {
-                tochk.remove(i);
-                i--;
-            }
-        }
-
-
-        //System.out.println(lastNode);
-        while(!foundPath)
+        while(!open.isEmpty() && !foundPath)
         {
-            tochk.sort(Comparator.comparing(Node::getCost));
-            if(tochk.isEmpty()) {
-                OutputConsole.get().writeLn("NOT FOUND");
+
+            System.out.println("--------------");
+            open.forEach(System.out::println);
+
+            Node q = open.poll();
+            if(q==null)
                 break;
-            }
-            int x = tochk.getFirst().x;
-            int y = tochk.getFirst().y;
-
-            gridSquares[y][x].setState(VISITED);
-            tab_pos[y][x] = 1;
-
+            gridSquares[q.y][q.x].setState(VISITED);
             Execution.get().Wait();
-            if( x != 0 )
-                if(tab_pos[y][x-1] == 0){
-                    tochk.add(new Node(x-1,y,tochk.getFirst()));
-                    tochk.getLast().setCost(startNode,lastNode);
-//                    tochk.getLast().setCost(startNode,
-//                            Math.pow(tochk.getLast().x - lastNode.x,2) + Math.pow(tochk.getLast().y- lastNode.y,2)
-//                    );
-                    gridSquares[y][x-1].setState(FRONTIER);
-                    tab_pos[y][x-1] = 1;
-                    Execution.get().Wait();
-                }
-                else if (tab_pos[y][x-1] == 10)
-                {
-                    lastNode.setPrev(tochk.getFirst());
-                    foundPath = true;
+            types[q.y][q.x] = 1;
+
+            ArrayList<Node> tochk = new ArrayList<>();
+            if(q.x+1 != cols)
+                if(types[q.y][q.x+1] != 5){
+                    tochk.add(new Node(q.x+1,q.y,q));
 
                 }
-            if( x != cols-1 )
-                if( tab_pos[y][x+1] == 0){
-                    tochk.add(new Node(x+1,y,tochk.getFirst()));
-                    tochk.getLast().setCost(startNode,lastNode);
-//                    tochk.getLast().setCost(startNode,Math.pow(tochk.getLast().x - lastNode.x,2)
-//                            + Math.pow(tochk.getLast().y- lastNode.y,2));
-                    gridSquares[y][x+1].setState(FRONTIER);
-                    tab_pos[y][x+1] = 1;
-                    Execution.get().Wait();
-                }else if (tab_pos[y][x+1] == 10)
-                {
-                    lastNode.setPrev(tochk.getFirst());
-                    foundPath = true;
 
-                }
-            if( y != 0 )
-                if(tab_pos[y-1][x] == 0){
-                    tochk.add(new Node(x,y-1,tochk.getFirst()));
-                    tochk.getLast().setCost(startNode,lastNode);
-//                    tochk.getLast().setCost(startNode,Math.pow(tochk.getLast().x - lastNode.x,2)
-//                            + Math.pow(tochk.getLast().y- lastNode.y,2));
-                    gridSquares[y-1][x].setState(FRONTIER);
-                    tab_pos[y-1][x]= 1;
-                    Execution.get().Wait();
-                } else if (tab_pos[y-1][x] == 10)
-                {
-                    lastNode.setPrev(tochk.getFirst());
-                    foundPath = true;
+            if(q.x-1 != -1)
+                if(types[q.y][q.x-1] != 5)
+                    tochk.add(new Node(q.x-1,q.y,q));
 
-                }
-            if( y !=rows-1 )
-                if(tab_pos[y+1][x] == 0){
-                    tochk.add(new Node(x,y+1,tochk.getFirst()));
-                    tochk.getLast().setCost(startNode,lastNode);
-//                    tochk.getLast().setCost(startNode,Math.pow(tochk.getLast().x - lastNode.x,2)
-//                            + Math.pow(tochk.getLast().y- lastNode.y,2));
-                    gridSquares[y+1][x].setState(FRONTIER);
-                    tab_pos[y+1][x] = 1;
-                    Execution.get().Wait();
-                }
 
-                else if (tab_pos[y+1][x] == 10)
-                {
-                    lastNode.setPrev(tochk.getFirst());
-                    foundPath = true;
 
-                }
-            tochk.removeFirst();
+            if(q.y+1 != rows)
+                if(types[q.y+1][q.x] != 5)
+                    tochk.add(new Node(q.x,q.y+1,q));
 
-        }
-        int i = 0;
-        Node tmp = lastNode;
-        if (foundPath)
-        {
-            while(lastNode != null)
+
+            if(q.y-1 != -1)
+                if(types[q.y-1][q.x] != 5)
+                    tochk.add(new Node(q.x,q.y-1,q));
+
+            for(Node tonode : tochk)
             {
-                i++;
-                gridSquares[lastNode.y][lastNode.x].setState(PATH);
+                if (tonode.x == lastNode.x && tonode.y == lastNode.y) {
+                    lastNode.prev = tonode;
+                    foundPath = true;
+                }
+                if(types[tonode.y][tonode.x] == 1)
+                    continue;
+                tonode.q = q.q +0.5;
+                // 0.5 == distance between one grid to next, never on a diagonal axis
+                tonode.h = Math.abs(tonode.x - lastNode.x)  + Math.abs(tonode.y - lastNode.y) ;
+                //Approximate of Heuristic distance, based on "Manhattan Distance" method
+                //tonode.h = Math.sqrt(Math.pow(tonode.x - lastNode.x,2)  + Math.pow(tonode.y - lastNode.y,2) );
+                //Approximate of Heuristic distance, based on real distance from node to DESTINATION
+                tonode.f = tonode.q + tonode.h;
+                boolean was = false;
+                for(Node toopen: open)
+                {
+                    if(toopen.x == tonode.x && toopen.y == tonode.y)
+                    {
+                        if(tonode.f < toopen.f)
+                        {
+                            toopen.f = tonode.f;
+                            toopen.q = tonode.q;
+                            toopen.h = tonode.h;
+                            toopen.prev = q;
+                        }
+                        was = true;
+                    }
+                }
+                if(was)
+                    continue;
+                gridSquares[tonode.y][tonode.x].setState(FRONTIER);
                 Execution.get().Wait();
-                lastNode = lastNode.prev;
+                open.add(tonode);
             }
+
         }
-        gridSquares[tmp.y][tmp.x].setState(DESTINATION);
+        Node tmpLast = lastNode;
+        int i = 0;
+        while(lastNode != null)
+        {
+            i++;
+            gridSquares[lastNode.y][lastNode.x].setState(PATH);
+            lastNode = lastNode.prev;
+        }
         gridSquares[startNode.y][startNode.x].setState(ORIGIN);
-        Execution.get().stopPoint(i);
+        gridSquares[tmpLast.y][tmpLast.x].setState(DESTINATION);
+        Execution.get().stopPoint(i-2);
     }
 }
